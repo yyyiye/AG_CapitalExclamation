01f5d7d1f4fdfa4b9264a573c430a8ec2e85aff8
yyyiye <475543548@qq.com>
2017-05-09 17:36:40 +0800

    Update MergeInfo.m

--- Preprocessing/MergeInfo.m
+++ Preprocessing/MergeInfo.m
@@ -3,11 +3,11 @@
 % [id]
 % [gender] 0 1 2
 % [Hat] 0:NA; 1:black; 2: blue; 3: brown; 4: green; 5: orenge; 6: red; 7:
-% gray; 8: white; 9: yellow; 10: clear
+% gray; 8: white; 9: yellow; 10: transparent; 11:multi-color;          
 % [Glasses] 0:NA; 1:black; 2: blue; 3: brown; 4: green; 5: orenge; 6: red; 7:
-% gray; 8: white; 9: yellow;
+% gray; 8: white; 9: yellow; 10: transparent; 11:multi-color;  
 % [Mask] 0:NA; 1:black; 2: blue; 3: brown; 4: green; 5: orenge; 6: red; 7:
-% gray; 8: white; 9: yellow;
+% gray; 8: white; 9: yellow; 10: transparent; 11:multi-color; 
 % [num] 0,1,2,3
 % [path] "collect1/[id].png","collect2/[id].png","collect2/[id].png"
 % [bbox] [x,y,w,h],[x,y,w,h],[x,y,w,h]
@@ -25,12 +25,110 @@ for k = 1 : length(No)
     %% gender
     gender = Gendercode(k);
     %% color
-    % Hat = Hat{k}, there should be number
-    Hat = 0;
-    % Glasses = Glasses{k}, there should be number
-    Glass = 0;
-    % Mask = Mask{k}, there should be number
-    Mask = 0;
+    hat = Hat{k};  %, there should be number
+%    string = strtrim(hat);  %count the number of colors
+%     counter = 0;
+%     while ~isempty(string)
+%         [~, string] = strtok(string);
+%         counter = counter + 1;
+%     end
+%     for i = 1 : counter
+    string = strtrim(hat); %get the first color
+    [string, ~] = strtok(string);
+        switch string   %judge the color
+            case {'N.A.','N.A','NA','N. A.'}
+                hat = 0;   
+            case 'BLACK'
+                hat = 1;
+            case 'BLUE'
+                hat = 2;
+            case 'BROWN'
+                hat = 3;
+            case 'GREEN'
+                hat = 4;
+            case 'ORANGE'
+                hat = 5;
+            case {'RED','ROSE'}
+                hat = 6;
+            case {'GREY','GRAY','SILVER'}
+                hat = 7;
+            case 'WHITE'
+                hat = 8;
+            case {'YELLOW','TAN','GOLD','GOLDEN'}
+                hat = 9;
+            case 'TRANSPARENT'  
+                hat = 10;
+            case 'MULTI'
+                hat = 11;         
+            otherwise 
+                hat = 99; %99 means can't judge
+        end 
+    %end
+    % Hat = 0;
+    glass = Glass{k};%;, there should be number
+    % Glass = 0;
+    string = strtrim(glass); %get the first color
+    [string, ~] = strtok(string);
+         switch string
+            case {'N.A.','N.A','NA'}
+                glass = 0;   
+            case 'BLACK'
+                glass = 1;
+            case 'BLUE'
+                glass = 2;
+            case 'BROWN'
+                glass = 3;
+            case 'GREEN'
+                glass = 4;
+            case 'ORANGE'
+                glass = 5;
+            case {'RED','ROSE'}
+                glass = 6;
+            case {'GREY','GRAY','SILVER'}
+                glass = 7;
+            case 'WHITE'
+                glass = 8;
+            case {'YELLOW','TAN','GOLD','GOLDEN'}
+                glass = 9;
+            case 'TRANSPARENT'  
+                glass = 10;
+            case 'MULTI'
+                glass = 11;
+            otherwise 
+                glass = 99;        
+         end   
+    mask = Mask{k};%, there should be number
+    % Mask = 0;
+    string = strtrim(mask); %get the first color
+    [string, ~] = strtok(string);
+         switch string
+            case {'N.A.','N.A','NA'}
+                mask = 0;   
+            case 'BLACK'
+                mask = 1;
+            case 'BLUE'
+                mask = 2;
+            case 'BROWN'
+                mask = 3;
+            case 'GREEN'
+                mask = 4;
+            case 'ORANGE'
+                mask = 5;
+            case {'RED','ROSE'}
+                mask = 6;
+            case {'GREY','GRAY','SILVER'}
+                mask = 7;
+            case 'WHITE'
+                mask = 8;
+            case {'YELLOW','TAN','GOLD','GOLDEN'}
+                mask = 9;
+            case 'TRANSPARENT'
+                mask = 10;           
+            case 'MULTI'
+                mask = 11;  
+            otherwise 
+                mask = 99;       
+         end         
     %% path and boxes
     path = {};boxes={};
     Name = [num2str(No(k)), '.png'];
@@ -48,7 +146,7 @@ for k = 1 : length(No)
     end
     num = length(path);
     if ~isempty(path)
-        out = num2str([id, gender, Hat,Glass, Mask,num]);        
+        out = num2str([id, gender, hat, glass, mask, num]);        
         for l=1:num
             out = [out,' "', path{l},'" ',num2str(boxes{l}),' ' ];
         end
